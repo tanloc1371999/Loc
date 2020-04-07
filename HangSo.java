ce810225a9675bd6888bfe74ca69c446bf4d1d77
tanloc1371999 <62705831+tanloc1371999@users.noreply.github.com>
2020-04-07 15:41:17 +0700

    Add files via upload

---
+++ HangSo.java
@@ -0,0 +1,18 @@
+
+public class HangSo {
+
+	public static final int HANG_SO = 4;
+	
+	public static final float SO_PI = 3.14f;
+	
+	public static final String TRUNG_TAM_JAVA = "Trung Tam Java";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("XIN CHAO" + TRUNG_TAM_JAVA);
+		
+		float chuVi= SO_PI *2;
+		System.out.println("chu vi duong  tron " + chuVi);
+	}
+
+}
